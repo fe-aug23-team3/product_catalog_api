62c28f2f4eaaddee1fc944159125e9fd294ee06e
Yaroslav <slavik2607.lviv@gmail.com>
2023-12-11 19:05:30 +0200

    Server is working correctly

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import express from 'express';
-
+import cors from 'cors';
 import { phoneRouter } from './routes/phone.route';
 
 function createServer() {
   const app = express();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+    }),
+  );
+
+  app.use('/public', express.static('public'));
 
   app.use('/phones', express.json(), phoneRouter);
 
   return app;
 }
 
-createServer().listen(3005, () => {
-  console.log('server is running on http://localhost3000');
+const PORT = 3005;
+
+createServer().listen(PORT, () => {
+  console.log(`🔥🚀🚀🚀 Server is running on http://localhost${PORT} 🔥🚀🚀🚀`);
 });
