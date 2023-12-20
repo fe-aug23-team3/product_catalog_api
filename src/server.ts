77cda092a58a827293559738c7a8000ab51c0da8
Yarolav <slavik2607.lviv@gmail.com>
2023-12-20 23:10:59 +0200

    fix cors for deploy

--- src/server.ts
+++ src/server.ts
@@ -14,14 +14,14 @@ function createServer() {
 
   app.use(
     cors({
-      origin: [
-        'https://fe-aug23-team3.github.io/product_catalog',
-        'http://localhost:3000',
-      ],
+      origin: ['https://fe-aug23-team3.github.io', 'http://localhost:3000'],
     }),
   );
 
   app.use('/public', express.static(path.resolve('../public')));
+
+  // app.use('/tablets', express.json(), tabletsRouter);
+  // app.use('/accessories', express.json(), accessoriesRouter);
 
   app.use('/phones', express.json(), phoneRouter);
 
